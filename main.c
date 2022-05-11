8fa6d08b7160b0649c33173c7f41b5c73a98b5bb
slucas <slucas@student.42mulhouse.fr>
2022-05-11 08:59:00 +0200

    put_ptr ok

--- main.c
+++ main.c
@@ -6,59 +6,23 @@
 /*   By: slucas <slucas@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 06:50:18 by slucas            #+#    #+#             */
-/*   Updated: 2022/05/05 12:30:12 by slucas           ###   ########.fr       */
+/*   Updated: 2022/05/11 06:25:19 by slucas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-void	ft_put_ptr(uintptr_t num)
-{
-	if (num >= 16)
-	{
-		ft_put_ptr(num / 16);
-		ft_put_ptr(num % 16);
-	}
-	else
-	{
-		if (num <= 9)
-			ft_putchar_fd((num + '0'), 1);
-		else
-			ft_putchar_fd((num - 10 + 'a'), 1);
-	}
-}
-
-int	ft_print_ptr2(unsigned long ptr)
-//int	ft_print_ptr2(unsigned long ptr)
-{
-	int i;
-
-	i = 0;
-	i += write(1, "0x", 2);
-	//if (ptr == 0)
-	if (!ptr)
-		i += write(1, "0", 1);
-	else
-	{
-		ft_put_ptr(ptr);
-	}
-	return (i);
-}
-
 int	main(void)
 {
 	unsigned long t;
-	t = 25698;
+	t = 20500;
 
-	char	*p = "coucou";
-	int		res = printf("Hello %p\n", p);
-	int		res2 = ft_printf("Hello %p\n", p);
+	int		res = printf("Hello %p\n", &t);
+	int		res2 = ft_printf("Hello %p\n", &t);
 
+	printf("--- %c ---\n", 20500 / 4096 + '0');
 	printf("res = %d\n", res);
 	printf("res2 = %d\n", res2);
-	printf("---------\n");
-	printf("vraie: %p\n", &t);
-	ft_put_ptr(t);
 	return (0);
 }
 
--- sources/ft_print_ptr.c
+++ sources/ft_print_ptr.c
@@ -6,45 +6,32 @@
 /*   By: slucas <slucas@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 08:23:57 by slucas            #+#    #+#             */
-/*   Updated: 2022/05/05 12:19:34 by slucas           ###   ########.fr       */
+/*   Updated: 2022/05/11 08:51:34 by slucas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int	ft_putptr(unsigned long p)
+static int	ft_putaddr(unsigned long p)
 {
-	/*
-	int			i;
-	uintptr_t	*n;
+	long	div;
+	int		i;
+	char	c;
 
+	div = 16;
 	i = 0;
-	n = &p;
-	//if (p == 0)
-	if (!p)
-		//i += write(1, '0', 1);
-		i += write(1, "0", 1);
-		//write(1, "0", 1);
-	else
+	i += write(1, "0x", 2);
+	while (p / div >= 16)
+		div *= 16;
+	while (div > 0)
 	{
-		if (n >= 16)
-		{
-			ft_putptr(n / (unsigned long *)16);
-			ft_putptr(n % (unsigned long *)16);
-		}
-		else
-		{
-			if (n <= 9)
-				i += write(1, (n + '0'), 1);
-				//write(1, (n + '0'), 1);
-			else
-				i += write(1, (n - 10 + 'a'), 1);
-				//write(1, (n - 10 + 'a'), 1);
-		}
+		c = '0' + p / div;
+		if (c > '9')
+			c += 39;
+		i += write(1, &c, 1);
+		p %= div;
+		div /= 16;
 	}
-	*/
-	(void)p;
-	int	i = 1;
 	return (i);
 }
 
@@ -53,6 +40,5 @@ void	ft_print_ptr(t_flags *flags)
 	unsigned long	p;
 
 	p = va_arg(flags->ap, unsigned long);
-	flags->length += write(1, "0x", 2);
-	flags->length += ft_putptr(p);
+	flags->length += ft_putaddr(p);
 }
