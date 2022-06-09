c627e713a43a76a080c52bbb21830aadccd36974
slucas <slucas@student.42mulhouse.fr>
2022-06-09 18:45:12 +0200

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slucas <slucas@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 06:50:18 by slucas            #+#    #+#             */
-/*   Updated: 2022/05/18 13:28:03 by slucas           ###   ########.fr       */
+/*   Updated: 2022/06/09 16:28:35 by slucas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	main(void)
 	printf("--- %c ---\n", 20500 / 4096 + 'a');
 	printf("res = %d\n", res);
 	printf("res2 = %d\n", res2);
+
+	printf("%.5d\n", 5);
 	return (0);
 }
 
